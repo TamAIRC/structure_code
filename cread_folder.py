227ea0de0d83fdef4fd0dc6d1d9873a996106284
NgoTamAIRC <133946215+Moobbot2@users.noreply.github.com>
2024-04-09 12:48:48 +0700

    udpate v2

--- cread_folder.py
+++ cread_folder.py
@@ -1,5 +1,6 @@
 import os
 
+
 def create_project_structure(project_name):
     # Tạo thư mục gốc
     os.makedirs(project_name)
@@ -16,7 +17,8 @@ def create_project_structure(project_name):
         "examples",
         "docker",
         "scripts",
-        "tools"
+        "tools",
+        "output"
     ]
 
     for d in dirs:
@@ -28,20 +30,32 @@ def create_project_structure(project_name):
         "requirements.txt",
         "setup.py",
         ".gitignore",
-        "config/config.ini",
+        "config/config.py",
+        "config/__init__.py",
         "src/package_name/__init__.py",
         "src/package_name/module1.py",
-        "src/package_name/module2.py",
         "src/tests/__init__.py",
         "src/tests/test_module1.py",
-        "src/tests/test_module2.py",
-        "docker/Dockerfile"
+        "docker/Dockerfile",
+        "log/.gitignore",
+        "models/.gitignore",
+        "output/.gitignore",
+        "tools/__init__.py",
         # Thêm các tệp tin khác nếu cần
     ]
+    # Content for the main .gitignore file
+    main_gitignore_content = """.conda\n.venv\n__pycache__\npoppler-*"""
 
+    # Content for the nested .gitignore files
+    nested_gitignore_content = "*\n!.gitignore"
     for f in files:
-        with open(os.path.join(project_name, f), "w") as file:
-            pass
+        file_path = os.path.join(project_name, f)
+        with open(file_path, "w") as file:
+            if f == ".gitignore":
+                file.write(main_gitignore_content)
+            elif f.endswith(".gitignore"):
+                file.write(nested_gitignore_content)
+
 
 # Sử dụng hàm để tạo cấu trúc thư mục
-create_project_structure("project_name")
+create_project_structure("text_summarizer")
