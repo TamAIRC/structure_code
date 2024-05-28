c4aef538dec322dd3d13e32f22eed2e6fcb438e5
TamAIRC <155130804+TamAIRC@users.noreply.github.com>
2024-05-28 18:02:35 +0700

    Update db_config.py
    thay doi noi dung chua trong database config

--- basic_project/configs/db_config.py
+++ basic_project/configs/db_config.py
@@ -1,6 +1,8 @@
 # configs/db_config.py
+# chua thong tin ve database server va quyen truy cap database (chu y, moi mot so dung dung quyen nay duoc thiet lap bang setup tai thoi diem trien khai
 CONNECT = {
     'URL': 'mongodb://localhost:27017',
     'DATABASE': 'dtu',
-    'QUESTION_COLLECTION': 'questions'
+    'USER': '?????',
+    'PASSWORD': '???????'
 }
